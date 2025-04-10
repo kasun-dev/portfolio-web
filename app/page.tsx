453a67432079578c3279843ad6054a59542f2e92
Kasun Sampath <ks2005755@gmail.com>
2025-04-10 23:28:53 +0530

    loding screen commit

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,13 @@ function Page() {
   }, []);
 
   if (isMobile === null) {
-    return <div className="text-center p-4">Loading...</div>;
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-#0a0a0a">
+      <div className="w-10 h-10 border-4 border-yellow-400 border-t-transparent rounded-full animate-spin">
+      </div>
+      </div>
+      
+    );
   }
 
   return (
