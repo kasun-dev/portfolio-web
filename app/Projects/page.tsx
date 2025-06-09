0c3ac2c9473b6ad8af3b669a0960c8829dcadd3d
Kasun Sampath <ks2005755@gmail.com>
2025-06-09 11:28:35 +0530

    Commit Project page new update

---
+++ app/Projects/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import NavBar from "../Components/NavBar";
+
+const ProjectPage = () => {
+
+    return (
+        <div>
+            <NavBar />
+        </div>
+
+    )
+}
+export default ProjectPage;
